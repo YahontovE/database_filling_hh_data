abc513fab16870cc7fcdb37354d93ececf611842
Evgen <yahontov-sport@inbox.ru>
2023-08-03 15:00:41 +0300

    add cod in func

--- src/postgres_db.py
+++ src/postgres_db.py
@@ -1,34 +1,41 @@
 import psycopg2
-class DBManager():
-    def get_companies_and_vacancies_count():
+from config import config
+
+
+class DBManager:
+
+    def __init__(self, database_name='hh_employer', params=config()):
+        self.database_name = database_name
+        self.params = params
+        #self.conn = psycopg2.connect(dbname=self.database_name, **self.params)
+
+    def get_companies_and_vacancies_count(self):
         '''получает список всех компаний и количество вакансий у каждой компании.'''
-        pass
-    def get_all_vacancies():
+        conn = psycopg2.connect(dbname=self.database_name,**self.params)
+        with conn.cursor() as cur:
+            cur.execute("SELECT title_employer,count(vacancies.title_vacancy) "
+                        "FROM employer JOIN vacancies USING(employer_id) "
+                        "GROUP BY title_employer")
+            rows = cur.fetchall()
+        conn.close()
+        return rows
+
+    def get_all_vacancies(self):
         '''получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию'''
         pass
 
-    def get_avg_salary():
+    def get_avg_salary(self):
         '''получает среднюю зарплату по вакансиям.'''
         pass
 
-    def get_vacancies_with_higher_salary():
+    def get_vacancies_with_higher_salary(self):
         '''получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.'''
         pass
-    def get_vacancies_with_keyword():
+
+    def get_vacancies_with_keyword(self):
         '''получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”.'''
         pass
-    def get_data_from_API(self):
-        '''Метод получения данных по API'''
-        v_hh = []
-        URL = 'https://api.hh.ru/employers'
-        for page in range(1, 11):
-            params = {
-                'per_page': 100,
-                'area': 113,
-                'page': page,
-                'text': self.user_request,
-                'only_with_salary': True
-            }
-            response = requests.get(URL, params=params).json()
-            v_hh.extend(response['items'])
-        return v_hh
+
+
+#q=DBManager('hh_employer',parm)
+#print(q.get_companies_and_vacancies_count())
