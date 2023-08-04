37e346ee93e29a1a4e8305d5d6bf984cbb351f57
Evgen <yahontov-sport@inbox.ru>
2023-08-04 12:23:42 +0300

    add sql requests

--- src/queries.sql
+++ src/queries.sql
@@ -6,16 +6,19 @@ GROUP BY title_employer
 
 --2 получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию
 
+SELECT employer.title_employer,vacancies.title_vacancy,
+vacancies.salary_from,vacancies.vacancy_url FROM vacancies
+JOIN employer USING(employer_id)
 
 --3 получает среднюю зарплату по вакансиям.
 
-SELECT round(AVG(salary)) FROM vacancies
+SELECT round(AVG(salary_from)) FROM vacancies
 
 --4 получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
 
-SELECT * from vacancies
-WHERE salary > (SELECT round(AVG(salary)) from vacancies)
+SELECT title_vacancy from vacancies
+WHERE salary_from > (SELECT round(AVG(salary_from)) from vacancies)
 
 --5 получает список всех вакансий, в названии которых содержатся переданные в метод слова
 
-SELECT * FROM vacancies WHERE title_vacancy LIKE '%Python%'
+SELECT title_vacancy FROM vacancies WHERE title_vacancy LIKE '%Python%'
