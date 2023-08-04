b442624fa70c850532a173707680fdf5092f2f62
Evgen <yahontov-sport@inbox.ru>
2023-08-04 12:23:26 +0300

    add cod for sql requests

--- src/postgres_db.py
+++ src/postgres_db.py
@@ -1,41 +1,60 @@
 import psycopg2
-from config import config
 
 
 class DBManager:
 
-    def __init__(self, database_name='hh_employer', params=config()):
+    def __init__(self, database_name='hh_employer'):
         self.database_name = database_name
-        self.params = params
-        #self.conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        self.conn = psycopg2.connect(host='localhost', database=self.database_name,
+                                     user='postgres', password='Vidineeva')
 
     def get_companies_and_vacancies_count(self):
         '''получает список всех компаний и количество вакансий у каждой компании.'''
-        conn = psycopg2.connect(dbname=self.database_name,**self.params)
-        with conn.cursor() as cur:
+        with self.conn.cursor() as cur:
             cur.execute("SELECT title_employer,count(vacancies.title_vacancy) "
                         "FROM employer JOIN vacancies USING(employer_id) "
                         "GROUP BY title_employer")
             rows = cur.fetchall()
-        conn.close()
+        self.conn.close()
         return rows
 
     def get_all_vacancies(self):
-        '''получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию'''
-        pass
+        '''получает список всех вакансий с указанием названия компании,
+         названия вакансии и зарплаты и ссылки на вакансию'''
+        with self.conn.cursor() as cur:
+            cur.execute("SELECT employer.title_employer,vacancies.title_vacancy,"
+                        "vacancies.salary_from,vacancies.vacancy_url"
+                        " FROM vacancies JOIN employer USING(employer_id)")
+            rows = cur.fetchall()
+        self.conn.close()
+        return rows
 
     def get_avg_salary(self):
         '''получает среднюю зарплату по вакансиям.'''
-        pass
+        with self.conn.cursor() as cur:
+            cur.execute("SELECT round(AVG(salary_from)) FROM vacancies")
+            rows = cur.fetchall()
+        self.conn.close()
+        return rows
 
     def get_vacancies_with_higher_salary(self):
         '''получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.'''
-        pass
+        with self.conn.cursor() as cur:
+            cur.execute("SELECT * from vacancies "
+                        "WHERE salary_from > (SELECT round(AVG(salary_from)) FROM vacancies)")
+            rows = cur.fetchall()
+        self.conn.close()
+        return rows
 
     def get_vacancies_with_keyword(self):
-        '''получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”.'''
-        pass
+        '''получает список всех вакансий,
+         в названии которых содержатся переданные в метод слова, например “python”.'''
+        with self.conn.cursor() as cur:
+            cur.execute("SELECT * FROM vacancies WHERE title_vacancy LIKE '%Python%'")
+            rows = cur.fetchall()
+        self.conn.close()
+        return rows
 
 
-#q=DBManager('hh_employer',parm)
-#print(q.get_companies_and_vacancies_count())
+q=DBManager()
+print(q.get_all_vacancies())
